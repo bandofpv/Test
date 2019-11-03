967e947fa35f4568c2d05b53af6f28141e1cce7b
bandofpv <andobernas@gmail.com>
2019-11-02 20:52:12 -0700

    added more stuff... v2!

---
+++ detect_image_copy.py
@@ -0,0 +1,159 @@
+# python3
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Example using TF Lite to detect objects in a given image."""
+
+import argparse
+import time
+
+from PIL import Image
+from PIL import ImageDraw
+
+import detect
+import tflite_runtime.interpreter as tflite
+
+from imutils.video import FPS
+from imutils.video import VideoStream
+
+import imutils
+import cv2
+import numpy as np
+
+
+EDGETPU_SHARED_LIB = 'libedgetpu.so.1'
+
+
+def load_labels(path, encoding='utf-8'):
+  """Loads labels from file (with or without index numbers).
+
+  Args:
+    path: path to label file.
+    encoding: label file encoding.
+  Returns:
+    Dictionary mapping indices to labels.
+  """
+  with open(path, 'r', encoding=encoding) as f:
+    lines = f.readlines()
+    if not lines:
+      return {}
+
+    if lines[0].split(' ', maxsplit=1)[0].isdigit():
+      pairs = [line.split(' ', maxsplit=1) for line in lines]
+      return {int(index): label.strip() for index, label in pairs}
+    else:
+      return {index: line.strip() for index, line in enumerate(lines)}
+
+
+def make_interpreter(model_file):
+  model_file, *device = model_file.split('@')
+  return tflite.Interpreter(
+      model_path=model_file,
+      experimental_delegates=[
+          tflite.load_delegate(EDGETPU_SHARED_LIB,
+                               {'device': device[0]} if device else {})
+      ])
+
+
+def draw_objects(draw, objs, labels):
+  """Draws the bounding box and label for each object."""
+  for obj in objs:
+    bbox = obj.bbox
+    draw.rectangle([(bbox.xmin, bbox.ymin), (bbox.xmax, bbox.ymax)],
+                   outline='red')
+    draw.text((bbox.xmin + 10, bbox.ymin + 10),
+              '%s\n%.2f' % (labels.get(obj.id, obj.id), obj.score),
+              fill='red')
+
+
+def main():
+  parser = argparse.ArgumentParser(
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument('-m', '--model', required=True,
+                      help='File path of .tflite file.')
+  parser.add_argument('-l', '--labels',
+                      help='File path of labels file.')
+  parser.add_argument('-t', '--threshold', type=float, default=0.4,
+                      help='Score threshold for detected objects.')
+  parser.add_argument('-o', '--output',
+                      help='File path for the result image with annotations')
+  parser.add_argument('-c', '--count', type=int, default=5,
+                      help='Number of times to run inference')
+  args = parser.parse_args()
+
+  labels = load_labels(args.labels) if args.labels else {}
+  interpreter = make_interpreter(args.model)
+  interpreter.allocate_tensors()
+
+  # initialize the video stream and allow the camera sensor to warmup
+  print("[INFO] starting video stream...")
+  vs = VideoStream(src=0).start() 
+  #vs = VideoStream(usePiCamera=False).start()
+  time.sleep(2.0)
+  fps = FPS().start()
+  
+  while True:
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 500 pixels
+    img = vs.read()
+    img = imutils.resize(img, width=500)
+
+    # prepare the frame for object detection by converting (1) it
+    # from BGR to RGB channel ordering and then (2) from a NumPy
+    # array to PIL image format
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = Image.fromarray(img)
+    
+    image = img
+    scale = detect.set_input(interpreter, image.size,
+                             lambda size: image.resize(size, Image.ANTIALIAS))
+  
+    print('----INFERENCE TIME----')
+    print('Note: The first inference is slow because it includes',
+          'loading the model into Edge TPU memory.')
+    for _ in range(args.count):
+      start = time.monotonic()
+      interpreter.invoke()
+      inference_time = time.monotonic() - start
+      objs = detect.get_output(interpreter, args.threshold, scale)
+  
+    # print('-------RESULTS--------')
+    # if not objs:
+      # print('No objects detected')
+  
+    # for obj in objs:
+      # print(labels.get(obj.id, obj.id))
+      # print('  id:    ', obj.id)
+      # print('  score: ', obj.score)
+      # print('  bbox:  ', obj.bbox)
+  
+    
+    draw_objects(ImageDraw.Draw(image), objs, labels)
+    image.save('yeet.jpg')
+    #image.show()
+    
+    image = cv2.imread('yeet.jpg')
+    
+    # show the output frame and wait for a key press
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+      break
+      
+    fps.update()
+
+
+if __name__ == '__main__':
+  main()
---
+++ detect_video_tflite.py
@@ -0,0 +1,174 @@
+# USAGE
+# python detect_video.py --model mobilenet_ssd_v2/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite --labels mobilenet_ssd_v2/coco_labels.txt
+
+# import the necessary packages
+from imutils.video import FPS
+from imutils.video import VideoStream
+import argparse
+import imutils
+import time
+import cv2
+import platform
+import subprocess
+from edgetpu.basic.basic_engine import BasicEngine
+from edgetpu.utils import dataset_utils, image_processing
+from PIL import Image
+from PIL import ImageDraw
+import numpy as np
+import tflite_runtime.interpreter as tflite
+import detect
+
+def create_pascal_label_colormap():
+  """Creates a label colormap used in PASCAL VOC segmentation benchmark.
+
+  Returns:
+    A Colormap for visualizing segmentation results.
+  """
+  colormap = np.zeros((256, 3), dtype=int)
+  indices = np.arange(256, dtype=int)
+
+  for shift in reversed(range(8)):
+    for channel in range(3):
+      colormap[:, channel] |= ((indices >> channel) & 1) << shift
+    indices >>= 3
+
+  return colormap
+
+def label_to_color_image(label):
+  """Adds color defined by the dataset colormap to the label.
+
+  Args:
+    label: A 2D array with integer type, storing the segmentation label.
+
+  Returns:
+    result: A 2D array with floating type. The element of the array
+      is the color indexed by the corresponding element in the input label
+      to the PASCAL color map.
+
+  Raises:
+    ValueError: If label is not of rank 2 or its value is larger than color
+      map maximum entry.
+  """
+  if label.ndim != 2:
+    raise ValueError('Expect 2-D input label')
+
+  colormap = create_pascal_label_colormap()
+
+  if np.max(label) >= len(colormap):
+    raise ValueError('label value too large.')
+
+  return colormap[label]
+
+EDGETPU_SHARED_LIB = 'libedgetpu.so.1'
+
+def make_interpreter(model_file):
+  model_file, *device = model_file.split('@')
+  return tflite.Interpreter(
+      model_path=model_file,
+      experimental_delegates=[
+          tflite.load_delegate(EDGETPU_SHARED_LIB,
+                               {'device': device[0]} if device else {})
+      ])
+  
+interpreter = make_interpreter(args.model)
+interpreter.allocate_tensors()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model',help='Path of the segmentation model.',required=True)
+parser.add_argument('--input', help='File path of the input image.', required=False)
+parser.add_argument('--output', help='File path of the output image.')
+parser.add_argument('--keep_aspect_ratio',dest='keep_aspect_ratio',action='store_true',help=(
+	'keep the image aspect ratio when down-sampling the image by adding '
+    'black pixel padding (zeros) on bottom or right. '
+    'By default the image is resized and reshaped without cropping. This '
+    'option should be the same as what is applied on input images during '
+    'model training. Otherwise the accuracy may be affected and the '
+    'bounding box of detection result may be stretched.'))
+    
+parser.set_defaults(keep_aspect_ratio=False)
+args = parser.parse_args()
+
+# load the Google Coral object detection model
+print("[INFO] loading Coral model...")
+engine = BasicEngine(args.model)
+_, height, width, _ = engine.get_input_tensor_shape()
+
+# initialize the video stream and allow the camera sensor to warmup
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+#vs = VideoStream(usePiCamera=False).start()
+time.sleep(2.0)
+fps = FPS().start()
+
+# loop over the frames from the video stream
+while True:
+	# grab the frame from the threaded video stream and resize it
+	# to have a maximum width of 500 pixels
+	img = vs.read()
+	img = imutils.resize(img, width=500)
+	orig = img.copy()
+
+	# prepare the frame for object detection by converting (1) it
+	# from BGR to RGB channel ordering and then (2) from a NumPy
+	# array to PIL image format
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+	img = Image.fromarray(img)
+	
+	if args.keep_aspect_ratio:
+		resized_img, ratio = image_processing.resampling_with_original_ratio(
+			img, (width, height), Image.NEAREST)
+	else:
+		resized_img = img.resize((width, height))
+		ratio = (1., 1.)
+		
+	input_tensor = np.asarray(resized_img).flatten()
+	_, raw_result = engine.run_inference(input_tensor)
+	result = np.reshape(raw_result, (height, width))
+	new_width, new_height = int(width * ratio[0]), int(height * ratio[1])
+
+	# If keep_aspect_ratio, we need to remove the padding area.
+	result = result[:new_height, :new_width]
+	vis_result = label_to_color_image(result.astype(int)).astype(np.uint8)
+	vis_result = Image.fromarray(vis_result)
+
+	vis_img = resized_img.crop((0, 0, new_width, new_height))
+
+	# loop over the results
+	# Concat resized input image and processed segmentation results.
+	concated_image = Image.new('RGB', (new_width*2, new_height))
+	concated_image.paste(vis_img, (0, 0))
+	concated_image.paste(vis_result, (width, 0))
+	concated_image.save('yeet.jpg')
+			
+	# # Display result.
+	# if platform.machine() == 'x86_64':
+		# # For gLinux, simply show the image.
+		# concated_image.show()
+	# elif platform.machine() == 'armv7l':
+		# # For Raspberry Pi, you need to install 'feh' to display image.
+			# subprocess.Popen(['feh', 'yeet.jpg'])
+	# else:
+		# print('Please check ', 'yeet.jpg')
+	
+	image = cv2.imread('yeet.jpg')
+	#time.sleep(5)
+
+	# # show the output frame and wait for a key press
+	cv2.imshow("Frame", image)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+		
+	interpreter.get_output_details()
+		
+	fps.update()
+
+fps.stop()
+print("[INFO] elapse time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
